cd3ca0ee5fed00431a3256f43220b30e41f59f16
David Zager <david.j.zager@gmail.com>
2021-12-07 16:45:12 -0500

    chore: remove minikube startup scripts

--- hack/minikube-down.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-command -v minikube >/dev/null 2>&1 || ( echo "Minikube not installed. Please install minikube." && exit 1 )
-
-set -ex
-
-minikube stop --profile src
-minikube stop --profile dest
